76812dc5dae6b9ed8286b32ce841ad910c8e236f
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-22 09:39:35 -0500

    done with first project

---
+++ MilesToKilometers/MilesToKilometers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MilesToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asking for user input
+            Console.Write("Enter miles to convert to kilometers: ");
+            // recording user input
+            string response = Console.ReadLine();
+            // inputting a newline character
+            Console.Write(Environment.NewLine);
+            // tryparsing user input to a double, assuming it's valid
+            if (!double.TryParse(response, out double miles))
+            {
+                // when the user input is not a valid number return
+                Console.WriteLine("{0} is not a valid number.", response);
+            } else
+            {
+                // calling the method milesToKilometers if it's a valid number
+                Console.WriteLine("{0} miles is {1} kilometers", miles, milesToKilometers(miles));
+            }
+            // Calling Console.ReadLine() to keep the window open
+            Console.ReadLine();
+        }
+        static double milesToKilometers(double miles)
+        {
+            // does the conversion using the formula mile * 1.6
+            return miles * 1.6;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 C# Programming Chapter 2
 
 ## TODO
-- [ ] 06 - MilesToKilometers
+- [X] 06 - MilesToKilometers
 - [ ] 08 - ProjectRaises
 - [ ] 11 – Eggs
 - [ ] 13 – Dollars
