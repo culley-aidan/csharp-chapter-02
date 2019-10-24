015af361e2e8cd60b09e6fd2dc018003735e673c
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-24 08:18:03 -0500

    forgot demovariables programs

---
+++ DemoVariables/DemoVariables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace DemoVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anInt = -123;
+            uint anUnsignedInt = 567;
+            WriteLine("The int is {0} and the unsigned int is {1}.", anInt, anUnsignedInt);
+        }
+    }
+}
