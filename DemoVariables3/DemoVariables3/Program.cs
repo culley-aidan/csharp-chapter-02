52b83ce8d820c3325c7f92c1c015d9e8c81d4774
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-24 08:24:46 -0500

    finished DemoVariables3

---
+++ DemoVariables3/DemoVariables3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace DemoVariables3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 4;
+            bool isSixMore = 6 > value;
+
+            WriteLine("When value is {0} isSixMore is {1}", value, isSixMore);
+            
+            value = 35;
+            isSixMore = 6 > value;
+            
+            WriteLine("When value is {0} isSixMore is {1}", value, isSixMore);
+        }
+    }
+}
